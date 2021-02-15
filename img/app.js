5b5c1b4ad98f58d4a350649e9bac8d15cc6babf5
Victor <rusuvictor30@gmail.com>
2021-02-14 22:32:43 -0500

    Final

---
+++ img/app.js
@@ -0,0 +1,17 @@
+const deg = 6;
+const h = document.querySelector('#h');
+const m = document.querySelector('#m');
+const s = document.querySelector('#s');
+
+const myTime = () => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    
+    h.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
+    m.style.transform = `rotateZ(${mm}deg)`;
+    s.style.transform = `rotateZ(${ss}deg)`;  
+}
+
+setInterval(myTime);
